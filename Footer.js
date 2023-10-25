2ee996ae6e85550fbbaf4bcfa74e5b00456b9f9a
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-25 14:53:27 -0500

    Home page (user not logged in)

--- Footer.js
+++ Footer.js
@@ -3,12 +3,12 @@ let footerString = `
   <li><img src="/Resources/Images/FVVV-Logo.png" id="footer-logo"></li>
 </ul>
 <ul>
-  <li><h4>About The Company</h4></li>
+  <li><h4>About Us</h4></li>
   <li><a href="#">Contact Us</a></li>
   <li><a href="#">About Us</a></li>
 </ul>
 <ul>
-  <li><h4>Appointments & Messaging</h4></li>
+  <li><h4>Scheduling</h4></li>
   <li><a href="/MessagingMainPage.html">Message a Provider</a></li>
   <li><a href="/ProvidersMainPage.html">Find a Provider</a></li>
   <li><a href="/AppointmentsMainPage.html">Schedule an Appointment</a></li>
