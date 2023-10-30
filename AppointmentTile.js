7b308493ec9e6a08dc56873ba6ea722505bf0499
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-29 23:11:59 -0500

    Grammar fixes (i.e. tiles --> tile)

--- AppointmentTile.js
+++ AppointmentTile.js
@@ -32,7 +32,7 @@ let appointmentTileString = `<div class="tile full-width" id="appointment-id" >
 
 /* This will need refactoring for dynamically loading all appointments in the database */
 document.addEventListener("DOMContentLoaded", () => {
-  let tiles = document.querySelectorAll(".appointmentsTile");
+  let tiles = document.querySelectorAll(".appointmentTile");
   tiles.forEach((tile) => {
     tile.innerHTML = appointmentTileString;
   })
