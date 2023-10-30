36cf76b1e9567f1f587c7a90c1792d49c5590c29
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-30 08:18:18 -0500

    Pets page and styling fixes for tile class

---
+++ PetProfileTile.js
@@ -0,0 +1,10 @@
+let appointmentTileString = ``;
+
+/* Todo: To be completed */
+/* This will need refactoring for dynamically loading all appointments in the database */
+document.addEventListener("DOMContentLoaded", () => {
+  let tiles = document.querySelectorAll(".petTile");
+  tiles.forEach((tile) => {
+    tile.innerHTML = appointmentTileString;
+  })
+});
